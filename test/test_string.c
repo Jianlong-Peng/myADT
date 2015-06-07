402a287db5c49efc990176a7ba3c6f7c1ca9be6c
Jianlong-Peng <pjlxsda@163.com>
2015-06-07 21:55:15 +0800

    debug

--- test/test_string.c
+++ test/test_string.c
@@ -5,7 +5,7 @@
 #        Email: jlpeng1201@gmail.com
 #     HomePage: 
 #      Created: 2015-06-07 21:17:15
-#   LastChange: 2015-06-07 21:25:26
+#   LastChange: 2015-06-07 21:43:43
 #      History:
 =============================================================================*/
 #include <stdio.h>
@@ -16,8 +16,8 @@
 
 int main(int argc, char *argv[])
 {
-    char *s[MAX_S+1];
-    char *pattern[MAX_S+1];
+    char s[MAX_S+1];
+    char pattern[MAX_S+1];
     StringPtr sptr;
     size_t i;
 
@@ -25,20 +25,20 @@ int main(int argc, char *argv[])
         do {
             printf("string (q to quit): ");
             scanf("%s", s);
-        } while(strlen(s));
+        } while(strlen(s)==0);
         if(strlen(s)==1 && (s[0]=='q' || s[0]=='Q'))
             break;
         do {
             printf("pattern: ");
             scanf("%s", pattern);
-        } while(strlen(pattern));
+        } while(strlen(pattern)==0);
         sptr = string_from_cstr(s);
         i = string_find_cstr(sptr, pattern);
         if(i == string_size(sptr))
             printf("pattern not found\n");
         else
             printf("pattern being found at %d\n", i);
-        string_free(sptr);
+        string_free(&sptr);
     }
 
     return 0;
